b409256baee60657706c563c52ca7bd6d1932388
Talar25 <90799331+Talar25@users.noreply.github.com>
2023-03-17 15:00:07 +0100

    add succesfully done removing buttons to tasks of inbox

--- src/index.js
+++ src/index.js
@@ -5,22 +5,25 @@ import './icons/check.png'
 import './icons/inbox.png'
 import './icons/today.png'
 import './icons/week.png'
-import {createTask} from './modules/task.js'
-import { openTaskManager } from './modules/task.js';
-import { closeTaskManager } from './modules/task.js';
-import { removeTask } from './modules/task.js';
+// import {createTask} from './modules/task.js'
+// import { openTaskManager } from './modules/task.js';
+// import { closeTaskManager } from './modules/task.js';
+// import { removeTask } from './modules/task.js';
+import { task } from './modules/task.js';
 
 const confirm = document.getElementById('btn-confirm-task');
-confirm.addEventListener('click', createTask)
+confirm.addEventListener('click', task.createTask)
 
 const addTask = document.getElementById('btn-task');
-addTask.addEventListener('click', openTaskManager);
+addTask.addEventListener('click', task.openTaskManager);
 
 const closeTask = document.getElementById('btn-cancel-task')
-closeTask.addEventListener('click', closeTaskManager);
+closeTask.addEventListener('click', task.closeTaskManager);
 
-const checkbox = document.getElementById('checkbox');
-checkbox.addEventListener('click', (e)=> {
-    if(checkbox.checked) removeTask()
-    
-})
+// const taskEl = document.getElementById('task');
+
+// taskEl.addEventListener('click', (e)=> {
+//     if(e.target.classList.contains('remove-btn')){
+//         console.log("hej")
+//     }
+// })
--- src/modules/task.js
+++ src/modules/task.js
@@ -1,147 +1,133 @@
+export const task = (function () {
+    const taskArr = [];
+    let index = 0;
 
-
-const createElement = function (task, index){
-
-    const taskEl = document.createElement('div');
-    taskEl.classList.add('task')
-    
-
-    const taskText = document.createElement('div');;
-    taskText.classList.add('task-text');
-
-    const taskTitle = document.createElement('span');
-    taskTitle.classList.add('task-title');
-    taskTitle.textContent = task.title
-
-    const taskContent = document.createElement('p');
-    taskContent.classList.add('task-content');
-    taskContent.textContent = task.content;
-
-    const taskDate = document.createElement('p');
-    taskDate.classList.add('task-date')
-    taskDate.textContent = task.date;
-
-    const label = document.createElement('label');
-    label.classList.add('check-container')
-
-    const input = document.createElement('input')
-    input.type = 'checkbox';
-    input.id = 'checkbox';
-
-    const span = document.createElement('span');
-    span.classList.add('checkmark')
-    span.setAttribute("onclick", tasksArray.removeFromArray(`${index}`), false);   
-
-
-//     <label class="check-container">
-//     <input id="checkbox" type="checkbox">
-//     <span class="checkmark"></span>
-// </label>
-    label.appendChild(input)
-    label.appendChild(span)
-    taskText.appendChild(taskTitle);
-    taskText.appendChild(taskContent);
-    taskEl.appendChild(label)
-    taskEl.appendChild(taskText);
-    taskEl.appendChild(taskDate);
-
-    return taskEl;
-
-}
-
-export const tasksArray = (function () {
-    const arr = [];
-    const addToArray = (obj) =>{
-        arr.push(obj)
-        return arr;
-    };
+    const addToArray = (obj) => {
+        taskArr.push(obj)
+    }
 
     const removeFromArray = (index) => {
-        arr.splice(index, 1);
-        return arr;
-    };
+        taskArr.splice(index, 1)
+        return renderTasks();
+    }
 
-    const renderTasks = function (tasks) {
-        tasks.forEach((element, i) => {
-            const container = document.getElementById('container')
-            const createdEl = createElement(element, i);
+    const createEl = function (task, index){
+
+        const taskEl = document.createElement('div');
+        taskEl.classList.add('task')
+        
+    
+        const taskText = document.createElement('div');;
+        taskText.classList.add('task-text');
+    
+        const taskTitle = document.createElement('span');
+        taskTitle.classList.add('task-title');
+        taskTitle.textContent = task.title
+    
+        const taskContent = document.createElement('p');
+        taskContent.classList.add('task-content');
+        taskContent.textContent = task.content;
+    
+        const taskDate = document.createElement('p');
+        taskDate.classList.add('task-date')
+        taskDate.textContent = task.date;
+
+        const button = document.createElement('button');
+        button.classList.add('remove-btn');
+        button.setAttribute('remove-data', "");
+        button.addEventListener('click', function (index) {
+            removeFromArray(index)
+        })
+    
+        // const label = document.createElement('label');
+        // label.classList.add('check-container')
+    
+        // const input = document.createElement('input')
+        // input.type = 'checkbox';
+        // input.classList.add('checkbox');
+        // input.value = index;
+        // input.setAttribute("onclick", "task.removeFromArray(index);");
+        // const span = document.createElement('span');
+        // span.classList.add('checkmark');
+        // span.id = index;
+        // span.setAttribute("onclick", `tasksArray.removeFromArray(${index})`, false);   
+    
+    
+    //     <label class="check-container">
+    //     <input id="checkbox" type="checkbox">
+    //     <span class="checkmark"></span>
+    // </label>
+        // label.appendChild(input)
+        // label.appendChild(span)
+        taskText.appendChild(taskTitle);
+        taskText.appendChild(taskContent);
+        // taskEl.appendChild(label)
+        taskEl.appendChild(taskText);
+        taskEl.appendChild(taskDate);
+        taskEl.appendChild(button)
+    
+        return taskEl;
+    
+    }
+
+    const renderTasks = () =>{
+        const container = document.getElementById('container')
+        container.textContent = "";
+        const header = document.createElement('h2')
+        header.classList.add('heading-secondary');
+        header.textContent = 'Inbox'
+        container.appendChild(header)
+
+        taskArr.forEach((element, i) => {
+
+            const createdEl = createEl(element, i);
             container.appendChild(createdEl);
         });
     }
 
-    return {
-        arr,
-        addToArray,
-        removeFromArray,
-        renderTasks
+    const resetTaskManager = () =>{
+        const title = document.getElementById('add-task-title').value = "";
+        const content = document.getElementById('add-task-content').value = "";
+        const date = document.getElementById('add-date').value = "";
     }
 
-}());
-
-// const addToArray = function (obj) {
-//     const arr = [];
-//     arr.push(obj)
-
-//     return arr;
-// }
-
-// const renderTasks = function (tasks) {
-//     tasks.forEach((element, i) => {
-//         const container = document.getElementById('container')
-//         const createdEl = createElement(element, i);
-//         container.appendChild(createdEl);
-//     });
-// }
-
-const resetTaskManager = function () {
-    const title = document.getElementById('add-task-title').value = "";
-    const content = document.getElementById('add-task-content').value = "";
-    const date = document.getElementById('add-date').value = "";
-}
-
-
-export const createTask = function () {
-    //selecting DOM elements
+    const createTask =  () =>{
+        //selecting DOM elements
+        
+        const title = document.getElementById('add-task-title').value;
+        const content = document.getElementById('add-task-content').value;
+        const date = document.getElementById('add-date').value;
     
-    const title = document.getElementById('add-task-title').value;
-    const content = document.getElementById('add-task-content').value;
-    const date = document.getElementById('add-date').value;
-
-    const arr = tasksArray.addToArray({title, content, date});
-    console.log(arr)
+        addToArray({ title, content, date});
+        index++;
+        console.log(taskArr)
     
-//     <div id="task" class="task">
-//     <div class="task-text">
-//     <span id="task-title" class="task-title">Lorem</span>
-//     <p id="task-content" class="task-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi praesentium ex tempora est exercitationem. </p>
-// </div>
-//     <p id="task-date" class="task-date">No date</p>
-// </div>
+        resetTaskManager()
+        const taskManager = document.getElementById('add-task')
+        taskManager.classList.remove('show')
+    
+        return renderTasks()
+    
+    }
+
+    const openTaskManager = () => {
+        resetTaskManager();
+        const taskManager = document.getElementById('add-task')
+        taskManager.classList.toggle('show')
+    }
+
+    const closeTaskManager = () =>{
+        resetTaskManager();
+        const taskManager = document.getElementById('add-task')
+        taskManager.classList.remove('show')
+    }
+
+    return {
+        createTask,
+        openTaskManager,
+        closeTaskManager,
+        removeFromArray
+    }
 
 
-    resetTaskManager()
-    const taskManager = document.getElementById('add-task')
-    taskManager.classList.remove('show')
-
-    return tasksArray.renderTasks(arr)
-
-}
-
-
-
-export const openTaskManager = function () {
-    resetTaskManager();
-    const taskManager = document.getElementById('add-task')
-    taskManager.classList.toggle('show')
-}
-
-export const closeTaskManager = function () {
-    resetTaskManager();
-    const taskManager = document.getElementById('add-task')
-    taskManager.classList.remove('show')
-}
-
-
-export const removeTask = function () {
-    console.log(index)
-}
+}())
