49ad14d22f11c5fb2dce2ebb866998d4a4c2921b
Talar25 <90799331+Talar25@users.noreply.github.com>
2023-05-17 14:08:21 +0200

    Delete not used files

--- src/modules/today.js
+++
@@ -1,7 +0,0 @@
-import { task } from "./task";
-
-export const today = () => {
-    const header = document.querySelector('heading-secondary');
-    header.textContent = 'Today'
-    const todayArr = task.sortArray();
-}
